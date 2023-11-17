c89d025cd5e416832a8fdc34ea70cc04af104708
hcarminati <hscarminati@hotmail.com>
2023-11-17 14:57:04 -0500

    Get user by id

--- user/routes.js
+++ user/routes.js
@@ -22,6 +22,37 @@ function UsersRoutes(app) {
         db.users = db.users.map((u) => u.username === username ? { ...u, ...user } : u);
         res.sendStatus(204);
     });
+
+    // Get user by id
+    app.get("/api/users/id/:id", (req, res) => {
+        const { id } = req.params;
+        const user = db.users.find((user) => user.id == Number(id));
+
+        if (!user) {
+            res.status(404).json({ message: "User not found!" });
+        }
+
+        res.send(user);
+    });
+
+    // Update user by id
+    app.put("/api/users/id/:id", (req, res) => {
+        const { id } = req.params;
+        const updatedUserData = req.body; // New data for the user
+
+        // Find the user by ID in the database
+        const userIndex = db.users.findIndex((user) => user.id == Number(id));
+
+        if (userIndex === -1) {
+            // User not found
+            res.status(404).json({ message: "User not found!" });
+        } else {
+            // Update the user data
+            db.users[userIndex] = { ...db.users[userIndex], ...updatedUserData };
+
+            res.status(200).json({ message: "User updated successfully", user: db.users[userIndex] });
+        }
+    });
 }
 
 export default UsersRoutes;
